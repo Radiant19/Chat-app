c8ef52d0d99db53f0b3e346dd9b140bc18977e8b
Radiant19 <152776230+Radiant19@users.noreply.github.com>
2024-11-17 00:07:06 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/authRoutes.js"
+import messageRoutes from "./routes/messageRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
+
+import Connection from "./db/connecttoMongoose.js";
+import cookieParser from "cookie-parser";
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth",authRoutes)
+app.use("/api/messages",messageRoutes)
+app.use("/api/users",userRoutes)
+
+
+// app.get("/", (req, res) => {
+//   res.send("hello world!!!");
+// });
+
+
+app.listen(PORT, async () => {
+Connection(),
+console.log(`Server Running on port ${PORT}`)});
